086f13b86a26304cd582e75083c94d8d18752dd3
damienbod <damien_bod@hotmail.com>
2024-07-04 13:12:56 +0200

    update packages

--- FunctionCertificates/CreateSelfSignedCertificateConsole/Program.cs
+++ FunctionCertificates/CreateSelfSignedCertificateConsole/Program.cs
@@ -3,42 +3,40 @@ using CertificateManager.Models;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.IO;
-using System.Security.Cryptography.X509Certificates;
 
-namespace CreateSelfSignedCertsConsole
+namespace CreateSelfSignedCertsConsole;
+
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            var dnsName = "FunctionCertificate";
-            var serviceProvider = new ServiceCollection()
-                .AddCertificateManager()
-                .BuildServiceProvider();
+        var dnsName = "FunctionCertificate";
+        var serviceProvider = new ServiceCollection()
+            .AddCertificateManager()
+            .BuildServiceProvider();
 
-            string password = "1234";
-            var importExportCertificate = serviceProvider.GetService<ImportExportCertificate>();
-            var createClientServerAuthCerts = serviceProvider.GetService<CreateCertificatesClientServerAuth>();
+        string password = "1234";
+        var importExportCertificate = serviceProvider.GetService<ImportExportCertificate>();
+        var createClientServerAuthCerts = serviceProvider.GetService<CreateCertificatesClientServerAuth>();
 
-            // Client self signed certificate
-            var client = createClientServerAuthCerts.NewClientSelfSignedCertificate(
-                new DistinguishedName { 
-                    CommonName = "functionsCertAuth", 
-                    Country = "CH", 
-                    Organisation="damienbod" },
-                new ValidityPeriod { ValidFrom = DateTime.UtcNow, ValidTo = DateTime.UtcNow.AddYears(10) },
-                dnsName);
+        // Client self signed certificate
+        var client = createClientServerAuthCerts.NewClientSelfSignedCertificate(
+            new DistinguishedName { 
+                CommonName = "functionsCertAuth", 
+                Country = "CH", 
+                Organisation="damienbod" },
+            new ValidityPeriod { ValidFrom = DateTime.UtcNow, ValidTo = DateTime.UtcNow.AddYears(10) },
+            dnsName);
 
-            client.FriendlyName = "azure client certificate";
+        client.FriendlyName = "azure client certificate";
 
-            var clientCertInPfxBtyes =
-                importExportCertificate.ExportSelfSignedCertificatePfx(password, client);
-            File.WriteAllBytes("functionsCertAuth.pfx", clientCertInPfxBtyes);
+        var clientCertInPfxBtyes =
+            importExportCertificate.ExportSelfSignedCertificatePfx(password, client);
+        File.WriteAllBytes("functionsCertAuth.pfx", clientCertInPfxBtyes);
 
-            var clientCertInPEMBtyes = importExportCertificate.PemExportPfxFullCertificate(client);
-            File.WriteAllText("functionsCertAuth.pem", clientCertInPEMBtyes);
+        var clientCertInPEMBtyes = importExportCertificate.PemExportPfxFullCertificate(client);
+        File.WriteAllText("functionsCertAuth.pem", clientCertInPEMBtyes);
 
-            Console.WriteLine("Certificates exported to pfx and cer files");
-        }
+        Console.WriteLine("Certificates exported to pfx and cer files");
     }
 }
