50b2e940a60d73804127ae99a65f22483a03d6b7
damienbod <damien_bod@hotmail.com>
2024-07-05 12:55:51 +0200

    code clean up

--- FunctionCertificates/FunctionCertificateConsoleClient/Program.cs
+++ FunctionCertificates/FunctionCertificateConsoleClient/Program.cs
@@ -21,14 +21,14 @@ class Program
     {
         var cert = new X509Certificate2("functionsCertAuth.pfx", "1234");
         var azureRandomStringBasicUrl = "https://functioncertificate20240704202458.azurewebsites.net/api/RandomStringCertAuth";
-        return await CallApiXARRClientCertHeader(azureRandomStringBasicUrl, cert);
+        return await CallApiClientCertHeader(azureRandomStringBasicUrl, cert, false);
 
         //var cert = new X509Certificate2("client401.pfx", "1234");
         //var localRandomStringBasicUrl = "http://localhost:7108/api/RandomStringCertAuth";
-        //return await CallApiXARRClientCertHeader(localRandomStringBasicUrl, cert);
+        //return await CallApiXARRClientCertHeader(localRandomStringBasicUrl, cert, true);
     }
 
-    private static async Task<string> CallApiXARRClientCertHeader(string url, X509Certificate2 clientCertificate)
+    private static async Task<string> CallApiClientCertHeader(string url, X509Certificate2 clientCertificate, bool isLocalTesting)
     {
         try
         {
@@ -42,7 +42,11 @@ class Program
                 Method = HttpMethod.Get,
             };
 
-            request.Headers.Add("X-ARR-ClientCert", Convert.ToBase64String(clientCertificate.RawData));
+            // Only needed for testing on local host
+            if (isLocalTesting)
+            {
+                request.Headers.Add("X-ARR-ClientCert", Convert.ToBase64String(clientCertificate.RawData));
+            }
             var response = await client.SendAsync(request);
 
             var responseContent = await response.Content.ReadAsStringAsync();
@@ -59,28 +63,4 @@ class Program
             throw new ApplicationException($"Exception {e}");
         }
     }
-
-    // v3 or older
-    // Test Azure deployment (now using only X-ARR-ClientCert in v4 isolated version)
-    //private static async Task<string> CallAzureDeployedAPI(string url, X509Certificate2 clientCertificate)
-    //{
-    //    var handler = new HttpClientHandler();
-    //    handler.ClientCertificates.Add(clientCertificate);
-    //    var client = new HttpClient(handler);
-
-    //    var request = new HttpRequestMessage()
-    //    {
-    //        RequestUri = new Uri(url),
-    //        Method = HttpMethod.Get,
-    //    };
-    //    var response = await client.SendAsync(request);
-    //    var responseContent = await response.Content.ReadAsStringAsync();
-    //    if (response.IsSuccessStatusCode)
-    //    {
-    //        Console.WriteLine(responseContent);
-    //        return responseContent;
-    //    }
-
-    //    throw new ApplicationException($"Status code: {response.StatusCode}, Error: {response.ReasonPhrase}");
-    //}
 }
