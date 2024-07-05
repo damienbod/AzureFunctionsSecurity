aca61f3377998053f7f05390788511c19b7e480a
damienbod <damien_bod@hotmail.com>
2024-07-05 09:23:14 +0200

    Update Azure function network security

--- NeworkSecurity/FunctionNeworkSecurity/RandomStringFunction.cs
+++ NeworkSecurity/FunctionNeworkSecurity/RandomStringFunction.cs
@@ -1,48 +1,42 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
-using System;
 using System.Security.Cryptography;
 using System.Text.Encodings.Web;
 
-namespace FunctionNeworkSecurity
+namespace FunctionNeworkSecurity;
+
+public class RandomStringFunction
 {
-    public class RandomStringFunction
+    private readonly ILogger<RandomStringFunction> _logger;
+    private readonly MyConfigurationSecrets _myConfigurationSecrets;
+
+    public RandomStringFunction(ILoggerFactory loggerFactory,
+        IOptions<MyConfigurationSecrets> myConfigurationSecrets)
     {
-        private readonly ILogger _log;
-        private readonly MyConfigurationSecrets _myConfigurationSecrets;
+        _logger = loggerFactory.CreateLogger<RandomStringFunction>();
+        _myConfigurationSecrets = myConfigurationSecrets.Value;
+    }
 
-        public RandomStringFunction(ILoggerFactory loggerFactory,
-            IOptions<MyConfigurationSecrets> myConfigurationSecrets)
-        {
-            _log = loggerFactory.CreateLogger<RandomStringFunction>();
-            _myConfigurationSecrets = myConfigurationSecrets.Value;
-        }
+    [Function("RandomString")]
+    public IActionResult RandomString(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req)
+    {
+        _logger.LogInformation("C# HTTP trigger RandomStringAuthLevelAnonymous processed a request.");
 
-        [FunctionName("RandomString")]
-        public IActionResult RandomString(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req)
-        {
-            _log.LogInformation("C# HTTP trigger RandomStringAuthLevelAnonymous processed a request.");
+        return new OkObjectResult($"{_myConfigurationSecrets.MySecret}  {GetEncodedRandomString()}");
+    }
 
-            return new OkObjectResult($"{_myConfigurationSecrets.MySecret}  {GetEncodedRandomString()}");
-        }
+    private string GetEncodedRandomString()
+    {
+        var base64 = Convert.ToBase64String(GenerateRandomBytes(100));
+        return HtmlEncoder.Default.Encode(base64);
+    }
 
-        private string GetEncodedRandomString()
-        {
-            var base64 = Convert.ToBase64String(GenerateRandomBytes(100));
-            return HtmlEncoder.Default.Encode(base64);
-        }
-
-        private byte[] GenerateRandomBytes(int length)
-        {
-            using var randonNumberGen = new RNGCryptoServiceProvider();
-            var byteArray = new byte[length];
-            randonNumberGen.GetBytes(byteArray);
-            return byteArray;
-        }
+    private byte[] GenerateRandomBytes(int length)
+    {
+        return RandomNumberGenerator.GetBytes(length);
     }
 }
