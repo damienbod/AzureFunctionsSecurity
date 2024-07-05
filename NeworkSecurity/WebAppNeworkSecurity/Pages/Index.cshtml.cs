aa46cbe244caca936f73231f3c46fd68cb5850d1
damienbod <damien_bod@hotmail.com>
2024-07-05 07:11:13 +0200

    code clean up

--- NeworkSecurity/WebAppNeworkSecurity/Pages/Index.cshtml.cs
+++ NeworkSecurity/WebAppNeworkSecurity/Pages/Index.cshtml.cs
@@ -5,28 +5,27 @@ using Microsoft.Extensions.Logging;
 using System.Net.Http;
 using System.Threading.Tasks;
 
-namespace WebAppNeworkSecurity.Pages
+namespace WebAppNeworkSecurity.Pages;
+
+public class IndexModel : PageModel
 {
-    public class IndexModel : PageModel
+    private readonly ILogger<IndexModel> _logger;
+    private readonly IHttpClientFactory _clientFactory;
+    private readonly IConfiguration _configuration;
+
+    [BindProperty]
+    public string RandomString { get; set; }
+
+    public IndexModel(IHttpClientFactory clientFactory, IConfiguration configuration, ILogger<IndexModel> logger)
     {
-        private readonly ILogger<IndexModel> _logger;
-        private readonly IHttpClientFactory _clientFactory;
-        private readonly IConfiguration _configuration;
+        _logger = logger;
+        _clientFactory = clientFactory;
+        _configuration = configuration;
+    }
 
-        [BindProperty]
-        public string RandomString { get; set; }
-
-        public IndexModel(IHttpClientFactory clientFactory, IConfiguration configuration, ILogger<IndexModel> logger)
-        {
-            _logger = logger;
-            _clientFactory = clientFactory;
-            _configuration = configuration;
-        }
-
-        public async Task OnGetAsync()
-        {
-            var client = _clientFactory.CreateClient();
-            RandomString = await client.GetStringAsync(_configuration["FunctionsApiUrl"]);
-        }
+    public async Task OnGetAsync()
+    {
+        var client = _clientFactory.CreateClient();
+        RandomString = await client.GetStringAsync(_configuration["FunctionsApiUrl"]);
     }
 }
